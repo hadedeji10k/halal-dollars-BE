9b0a9ee9c27363fedcdb10f6d2c962170427c4c4
Yusuf Adedeji <adedejiyusuf26@gmail.com>
2024-12-19 22:40:00 +0100

    Added changes

--- src/api/routes/futureWealth.ts
+++ src/api/routes/futureWealth.ts
@@ -1,16 +1,53 @@
 import { FastifyInstance } from "fastify";
 import { Container } from "typedi";
-import { TotalReachController } from "../controllers";
+import { FutureWealthController } from "../controllers";
 import { auth } from "../middleware";
 
-export async function totalReach(app: FastifyInstance) {
-  const controller = Container.get(TotalReachController);
+export async function futureWealth(app: FastifyInstance) {
+  const controller = Container.get(FutureWealthController);
 
   app.get(
     "/",
     {
       onRequest: [auth.user()],
     },
-    controller.getUserTotalReach.bind(controller)
+    controller.getUserFutureWealths.bind(controller)
+  );
+  app.get(
+    "/:id",
+    {
+      onRequest: [auth.user()],
+    },
+    controller.getUserFutureWealth.bind(controller)
+  );
+
+  // subsidiaries
+  app.post(
+    "/subsidiary",
+    {
+      onRequest: [auth.user()],
+    },
+    controller.createSubsidiary.bind(controller)
+  );
+  app.put(
+    "/subsidiary/:id",
+    {
+      onRequest: [auth.user()],
+    },
+    controller.updateSubsidiary.bind(controller)
+  );
+  app.get(
+    "/subsidiary",
+    {
+      onRequest: [auth.user()],
+    },
+    controller.getUserSubsidiaries.bind(controller)
+  );
+  app.get(
+    "/subsidiary/:id",
+    {
+      onRequest: [auth.user()],
+    },
+    controller.getUserSubsidiary.bind(controller)
   );
 }
